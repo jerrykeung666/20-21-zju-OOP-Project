5df9a002d3d14340f7a2ba981263bf75a775910c
hhubibi <60770083+hhubibi@users.noreply.github.com>
2021-06-26 09:54:17 +0800

    Update strategy.h

--- main/strategy.h
+++ main/strategy.h
@@ -9,7 +9,7 @@ class Strategy
 public:
     Strategy(Player* player, const QVector<Card>& cards);
 
-    int countOfPoint(QVector<Card> cards, CardPoint point);
+    int countOfPoint(QVector<Card> &cards, CardPoint point);    // 计算牌数量
     QVector<Card> findSamePointCards(CardPoint point, int count);	// 找出count张点数为point的牌
     QVector<QVector<Card>> findCardsByCount(int count);		// 找出所有张数为count的所有牌数组
     QVector<Card> getRangeCards(CardPoint beginPoint, CardPoint endPoint);	// 根据点数范围找牌
@@ -26,6 +26,8 @@ public:
     QVector<Card> playBeatHand(CardGroups hand);		// 大过指定牌型
 
     bool whetherToBeat(const QVector<Card>& myCards);	// 能大过时，判断是打还是放行
+
+    QVector<Card> makeStragety();
 private:
     Player* player;
     QVector<Card> cards;
